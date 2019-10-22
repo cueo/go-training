0208071aebdfa920067028c0af19231ba6a45b08
cueo <mohit.ritanil@gmail.com>
2019-10-22 14:09:16 +0530

    Arrays in Go

---
+++ day01/arrays.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int[] n => Java
+	var numbers [10]int
+	fmt.Println(numbers)
+	// println(numbers)  // does not work ??
+
+	numbers[0] = 100
+	fmt.Println(numbers)
+
+	// numbers[11] = 100  // compile time error
+
+	langs := []string {"Go", "C", "Java", "Python", "Kotlin"}
+	fmt.Println(langs)
+
+	var arr = make([]int, 10)
+	println(arr)
+	fmt.Println(arr)
+}
