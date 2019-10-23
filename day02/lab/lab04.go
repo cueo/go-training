d64c6edd24e1b63adbfda0fee4d48c218f4acd23
cueo <mohit.ritanil@gmail.com>
2019-10-23 10:30:50 +0530

    Use regex to handle different spaces

--- day02/lab/lab04.go
+++ day02/lab/lab04.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	input := `
-409  194  207  470  178  454  235  333  511  103  474  293  525  372  408  428
+	input := `409  194  207  470  178  454  235  333  511  103  474  293  525  372  408  428
 4321  2786  6683  3921  265  262  6206  2207  5712  214  6750  2742  777  5297
 3536  2675  1298  1069  175  145  706  2614  4067  4377  146  134  1930  3850
 2169  1050  3705  2424  614  3253  222  3287  3340  2637  61  216  2894  247
@@ -26,15 +26,23 @@ func main() {
 func calculateChecksum(input string) (checksum int) {
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
-		line := strings.Trim(line, " ")
-		numStrings := strings.Split(line, " ")
+		numStrings := getLine(line)
+		// numStrings := strings.Split(line, "  ")
 		min, max := getMinMax(numStrings)
 		checksum += max - min
 	}
 	return
 }
 
+func getLine(line string) (numStrings []string) {
+	line = strings.Trim(line, " ")
+	r := regexp.MustCompile(` +`)
+	numStrings = r.Split(line, -1)
+	return
+}
+
 func getMinMax(numStrings []string) (min int, max int) {
+	// fmt.Println(numStrings, len(numStrings))
 	min, max = 9999, -9999
 	for _, numString := range numStrings {
 		num, _ := strconv.Atoi(numString)
@@ -45,5 +53,6 @@ func getMinMax(numStrings []string) (min int, max int) {
 			max = num
 		}
 	}
+	// fmt.Println(min, max)
 	return
 }
