bf35d1e1a8a19a341203d22c90e36d18c68eea91
cueo <mohit.ritanil@gmail.com>
2019-10-23 12:18:05 +0530

    Update higher order fn

--- day02/higher_order_functions.go
+++ day02/higher_order_functions.go
@@ -8,22 +8,26 @@ func main() {
 	var greeter Greeter = func(name string) string {
 		return "Hello, " + name
 	}
-	fmt.Println(greeter("momes"))
+	fmt.Println(1, greeter("momes"))
 	printGreetings(greeter, "momes")
 
-	fmt.Println(getGreeter()("momes"))
+	greet := getGreeter()
+	fmt.Println(3, greet("momes"))
+	fmt.Println(4, getGreeter()("momes"))
 	printGreetings(getGreeter(), "momes")
 }
 
 func getGreeter() (greeter Greeter) {
+	message := "Hello, "
 	greeter = func(name string) string {
-		return "Hello, " + name + "!"
+		message += name + "!"
+		return message
 	}
 	return
 }
 
 // higher order function
 func printGreetings(greeter Greeter, name string) {
-	fmt.Println(greeter(name))
+	fmt.Println("in", greeter(name))
 }
 
